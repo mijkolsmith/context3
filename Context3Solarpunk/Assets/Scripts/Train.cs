05deca8e7746dfb081d829e9ad6997ac659a3613
joris d <j_d_derks@hotmail.com>
2022-12-06 15:35:53 +0100

    add first load of quest progression, stops at crafting machine

--- Context3Solarpunk/Assets/Scripts/Train.cs
+++ Context3Solarpunk/Assets/Scripts/Train.cs
@@ -8,10 +8,12 @@ public class Train : MonoBehaviour, IInteractable
     [SerializeField] private PlayerControllerPointClick player;
 
     private SequenceManager sequenceManager;
-
+    [SerializeField] private bool isInteractable;
     //For outline
     private Outline objectOutline;
     private bool highlighting;
+
+    public bool IsInteractable { get => isInteractable; set => isInteractable = value; }
 
     private void Start()
     {
@@ -26,14 +28,31 @@ public class Train : MonoBehaviour, IInteractable
 
     public void Highlight(Color color)
     {
-        objectOutline.OutlineWidth = 5f;
-        objectOutline.OutlineColor = color;
-        highlighting = true;
+        if (isInteractable)
+        {
+            objectOutline.OutlineWidth = 5f;
+            objectOutline.OutlineColor = color;
+            highlighting = true;
+        } 
+        else
+        {
+            objectOutline.OutlineWidth = 5f;
+            objectOutline.OutlineColor = Color.red;
+            highlighting = true;
+        }
+    }
+
+    public void SetInteractable(bool isInteractable)
+    {
+        IsInteractable = isInteractable;
     }
 
     public void Interact()
     {
+        if (IsInteractable)
+        {
+            GameManager.Instance.SequenceManager.TimeTravel();
+        }
         //Do the sequencemanager time travel thing
-        GameManager.Instance.SequenceManager.TimeTravel();
     }
 }
