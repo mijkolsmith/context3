32ef6d84fb6195acee779db4a58589fcbe7dd57f
mijkolsmith <mijkolsmith@live.com>
2023-01-16 14:31:16 +0100

    working on introscene implementation

--- Context3Solarpunk/Assets/Scripts/IntroManager.cs
+++ Context3Solarpunk/Assets/Scripts/IntroManager.cs
@@ -1,13 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class IntroManager : MonoBehaviour
 {
-    [SerializeField] private List<string> introDialogue = new List<string>();
+    [SerializeField] private string[] introDialogue;
     int currentDialogueIndex;
+
+    [SerializeField] private Sprite[] introSprites;
+    [SerializeField] private Image introImage;
 
     [SerializeField] private TextMeshProUGUI dialogueText;
 
@@ -19,15 +20,16 @@ public class IntroManager : MonoBehaviour
     private void Start()
     {
         dialogueText.text = introDialogue[0];
+        introImage.sprite = introSprites[0];
     }
 
     /// <summary>
-    /// byAmount can be 1 to go ahead and -1 go go back
+    /// This method adjusts the dialogue visible on the screen, and also adjusts the buttons and background accordingly
     /// </summary>
-    /// <param name="byAmount"></param>
+    /// <param name="byAmount">1 to go ahead and -1 go back</param>
     public void ProgressDialogue(int byAmount)
     {
-        if ((currentDialogueIndex + byAmount) > -1 && (currentDialogueIndex + byAmount) < introDialogue.Count)
+        if ((currentDialogueIndex + byAmount) > -1 && (currentDialogueIndex + byAmount) < introDialogue.Length)
         {
             currentDialogueIndex += byAmount;
         }
@@ -35,16 +37,25 @@ public class IntroManager : MonoBehaviour
         if (currentDialogueIndex == 0)
         {
             previousButtonGameObject.SetActive(false);
-        } 
-        else
+            introImage.sprite = introSprites[0];
+        }
+        else 
         {
             previousButtonGameObject.SetActive(true);
+            if (currentDialogueIndex == 1)
+			{
+                introImage.sprite = introSprites[1];
+            }
+            else if (currentDialogueIndex == 3)
+			{
+                introImage.sprite = introSprites[2];
+            }
+            else introImage.sprite = introSprites[0];
         }
-
 
         dialogueText.text = introDialogue[currentDialogueIndex];
 
-        if (currentDialogueIndex == introDialogue.Count -1)
+        if (currentDialogueIndex == introDialogue.Length - 1)
         {
             sceneSwitchButton.gameObject.SetActive(true);
             nextButtonGameObject.SetActive(false);
